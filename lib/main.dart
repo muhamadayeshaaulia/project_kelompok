0e94ded6c381bca052139c6884133887c8a63957
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2025-11-30 18:56:48 +0700

    membuat page 2 dan 3 sama seperti page 1 dan merubah gambarnya untuk membedakan page 1 2 dan 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:project_kelompok/screen/home_screen.dart';
 import 'package:project_kelompok/screen/page1.dart';
+import 'package:project_kelompok/screen/page2.dart';
+import 'package:project_kelompok/screen/page3.dart';
 import 'package:project_kelompok/screen/splash_screen.dart';
 
 void main() {
@@ -20,6 +22,8 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const MySplashScreen(),
         '/page1': (context) => const MyPage1(),
+        '/page2': (context) => const MyPage2(),
+        '/page3': (context) => const MyPage3(),
         '/home': (context) => const HomeScreen(),
       },
     );
