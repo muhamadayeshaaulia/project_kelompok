2c427ff8d1936f04de80465029c57f547666372b
rozakgit <muhammadabdulrozak21@gmail.com>
2025-12-05 19:12:07 +0700

    Menambahkan Padding Pada sisi Halaman Login untuk membuat space, agar tidak terlalu mepet

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -7,55 +7,55 @@ class MyLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Login untuk melanjukan perjalan an mu',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Login untuk melanjukan perjalan an mu',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-          ),
-          Lottie.asset(
-            'assets/annimations/Bus_Loader.json',
-            height: 200,
-            width: 200,
-            fit: BoxFit.contain
-          ),
-          TextField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.blue),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              prefixIcon: Icon(Icons.email),
-              labelText: 'Email',
-              hintText: 'Masukan Email Anda',
+            Lottie.asset(
+              'assets/annimations/Bus_Loader.json',
+              height: 200,
+              width: 200,
+              fit: BoxFit.contain,
             ),
-          ),
-          SizedBox(height: 20),
-          TextField(
-            keyboardType: TextInputType.visiblePassword,
-             obscureText: true,
-             decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.blue),
-                borderRadius: BorderRadius.circular(20)
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                prefixIcon: Icon(Icons.email),
+                labelText: 'Email',
+                hintText: 'Masukan Email Anda',
               ),
-              prefixIcon: Icon(Icons.lock),
-              labelText: 'Password',
-              hintText: 'Masukan password anda',
-              suffixIcon: IconButton(
-                icon: Icon(Icons.visibility),
-                onPressed: (){},
-              )
-             ),
-          )
-        ],
+            ),
+            SizedBox(height: 20),
+            TextField(
+              keyboardType: TextInputType.visiblePassword,
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                prefixIcon: Icon(Icons.lock),
+                labelText: 'Password',
+                hintText: 'Masukan password anda',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.visibility),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
