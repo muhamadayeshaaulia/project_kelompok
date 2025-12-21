79eca08bafa5be9fcaa9716b776019e5a460676d
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2025-12-21 19:57:59 +0700

    membuat buildnavitem untuk row di atas

--- lib/widgats/custom_buttom_nav.dart
+++ lib/widgats/custom_buttom_nav.dart
@@ -17,35 +17,29 @@ class CustomButtomNav extends StatelessWidget {
           children: [
             Row(
               children: [
-                _buildNavItem(
-                  context,
-                  icon: Icons.home,
-                  index: 0,
-                ),
-                _buildNavItem(
-                  context,
-                  icon: Icons.people,
-                  index: 1,
-                ),
+                _buildNavItem(context, icon: Icons.home, index: 0),
+                _buildNavItem(context, icon: Icons.people, index: 1),
               ],
             ),
             Row(
               children: [
-                _buildNavItem(
-                  context,
-                  icon: Icons.info,
-                  index: 2,
-                ),
-                _buildNavItem(
-                  context,
-                  icon: Icons.person,
-                  index: 3,
-                ),
+                _buildNavItem(context, icon: Icons.info, index: 2),
+                _buildNavItem(context, icon: Icons.person, index: 3),
               ],
             ),
           ],
         ),
       ),
     );
+  }
+
+  Widget _buildNavItem(
+    BuildContext context,
+    IconData icon,
+    String label,
+    int index,
+  ) {
+    bool isActive = index == currentIndex;
+    return MaterialButton(minWidth: 40, onPressed: () {});
   }
 }
