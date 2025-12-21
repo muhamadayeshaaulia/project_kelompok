8bc613018e2caf0a3a135ead34c21d62e98d16ef
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2025-12-21 19:43:59 +0700

    membuat custom untuk nav

---
+++ lib/widgats/custom_buttom_nav.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomButtomNav extends StatelessWidget {
+  const CustomButtomNav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
