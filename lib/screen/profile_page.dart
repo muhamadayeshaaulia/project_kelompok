222bd64217f27b67330ea3a94b06a0ae7115c8ce
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2025-12-21 18:29:48 +0700

    merubah dari stateles ke statefull

--- lib/screen/profile_page.dart
+++ lib/screen/profile_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
