ad32ffc6ae0eeb92749cef812ec010093151e2fb
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2025-12-21 19:53:56 +0700

    membuat row di bagian kiri

--- lib/widgats/custom_buttom_nav.dart
+++ lib/widgats/custom_buttom_nav.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
 class CustomButtomNav extends StatelessWidget {
-  const CustomButtomNav({super.key});
+  final int currentIndex;
+  const CustomButtomNav({super.key, required this.currentIndex});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return BottomAppBar(
+      shape: const CircularNotchedRectangle(),
+      notchMargin: 8.0,
+      color: Colors.white,
+      child: SizedBox(
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                _buildNavItem(
+                  context,
+                  icon: Icons.home,
+                  index: 0,
+                ),
+                _buildNavItem(
+                  context,
+                  icon: Icons.people,
+                  index: 1,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
