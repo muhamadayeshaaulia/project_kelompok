f62537e9c8ea1aae112368ae7d1c891057f46123
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2025-12-29 22:52:58 +0700

    mengabil data following dan followers dari firebase serta menyiapkan searchquery untuk mencari user

--- lib/screen/following_page.dart
+++ lib/screen/following_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 class FollowingPage extends StatefulWidget {
   const FollowingPage({super.key});
@@ -8,8 +11,54 @@ class FollowingPage extends StatefulWidget {
 }
 
 class _FollowingPageState extends State<FollowingPage> {
+  final currentUser = FirebaseAuth.instance.currentUser;
+  String searchQuery = "";
+  List<String> myFollowingList = [];
+  List<String> myFollowersList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMyFollowing();
+    _loadMyFollowers();
+  }
+
+  Future<void> _loadMyFollowing() async {
+    if (currentUser == null) return;
+    final snapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(currentUser!.uid)
+        .collection('following')
+        .get();
+
+    if (mounted) {
+      setState(() {
+        myFollowingList = snapshot.docs.map((doc) => doc.id).toList();
+      });
+    }
+  }
+
+  Future<void> _loadMyFollowers() async {
+    if (currentUser == null) return;
+    final snapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(currentUser!.uid)
+        .collection('followers')
+        .get();
+
+    if (mounted) {
+      setState(() {
+        myFollowersList = snapshot.docs.map((doc) => doc.id).toList();
+      });
+    }
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      
+    );
   }
 }
