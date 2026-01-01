277357a82e1ea4dab90dca8743e43cf943cb2bfa
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-01-01 18:32:05 +0700

    setup file explore dengan statefull

---
+++ lib/screen/explor.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExplorPage extends StatefulWidget {
+  const ExplorPage({super.key});
+
+  @override
+  State<ExplorPage> createState() => _ExplorPageState();
+}
+
+class _ExplorPageState extends State<ExplorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
