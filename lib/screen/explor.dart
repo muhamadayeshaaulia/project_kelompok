9e4c599ea12e983ff5e35a8134f7cedc31350371
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-01-01 18:36:58 +0700

    memberikan logika untuk format waktu jika blm 24 jam makan akan tampil jam dan ketika sudah lebih dari 24 jam maka akan tampil tanggal bulan dan tahun

--- lib/screen/explor.dart
+++ lib/screen/explor.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:project_kelompok/detail/postingan.dart';
+import 'package:intl/intl.dart';
 
 class ExplorPage extends StatefulWidget {
   const ExplorPage({super.key});
@@ -8,6 +11,18 @@ class ExplorPage extends StatefulWidget {
 }
 
 class _ExplorPageState extends State<ExplorPage> {
+  String formatPostTime(Timestamp? timestamp) {
+    if (timestamp == null) return "...";
+    DateTime postDate = timestamp.toDate();
+    DateTime now = DateTime.now();
+    Duration difference = now.difference(postDate);
+    if (difference.inHours < 24) {
+      return DateFormat('HH:mm').format(postDate);
+    } else {
+      return DateFormat('d MMM yyyy').format(postDate);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
