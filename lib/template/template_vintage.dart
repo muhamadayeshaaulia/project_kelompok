8b63e8c6b3c1329152175db5d44e989cdde33f26
rozakgit <muhammadabdulrozak21@gmail.com>
2026-01-02 22:05:52 +0700

    update stf

--- lib/template/template_vintage.dart
+++ lib/template/template_vintage.dart
@@ -1,1 +1,25 @@
+import 'dart:io';
 
+import 'package:flutter/material.dart';
+
+class TemplateVintage extends StatefulWidget {
+  final List<File>? initialImages;
+
+  const TemplateVintage({super.key, this.initialImages});
+
+  @override
+  State<TemplateVintage> createState() => _TemplateVintageState();
+}
+
+class _TemplateVintageState extends State<TemplateVintage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Dashboard"), actions: const []),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(children: []),
+      ),
+    );
+  }
+}
