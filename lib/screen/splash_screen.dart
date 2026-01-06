1e4ee83a8cdf3cb5f07a2f5c49bf6739b4b23334
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-01-06 23:38:39 +0700

    merubah durasi proses loading lottie menjadi 2s

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -15,7 +15,7 @@ class _MySplashScreenState extends State<MySplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 1), () {
+    Future.delayed(const Duration(seconds: 2), () {
       final user = FirebaseAuth.instance.currentUser;
 
       if (mounted) {
