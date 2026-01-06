c242da6a163edd84886b7a30fb2492631b65fe9f
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-01-06 15:38:19 +0700

    mengimpor cloud firestore dan mengambil uid berdasarkan data dari firestore

--- lib/member/profil_ayesha.dart
+++ lib/member/profil_ayesha.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AyeshaProfilPage extends StatelessWidget {
   const AyeshaProfilPage({super.key});
+  final String ayeshaDocId = "6JozUEKn8fMzDjoq4ZyHtwqm8IP2";
 
   @override
   Widget build(BuildContext context) {
