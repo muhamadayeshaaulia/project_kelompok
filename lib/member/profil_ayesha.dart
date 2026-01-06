0536020b64a10471b1c7dd4b8f0f01f285237275
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-01-06 15:36:24 +0700

    membuat setup untuk profil ayesha

---
+++ lib/member/profil_ayesha.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AyeshaProfilPage extends StatelessWidget {
+  const AyeshaProfilPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screen/member_card.dart
+++ lib/screen/member_card.dart
@@ -54,7 +54,7 @@ class MemberCardPage extends StatelessWidget {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => const ProfilAyeshaPage(),
+                          builder: (context) => const AyeshaProfilPage(),
                         ),
                       );
                     },
