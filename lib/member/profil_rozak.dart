59a2dc2cba9371b8f2001e452bdda698a9a544fe
rozakgit <muhammadabdulrozak21@gmail.com>
2026-01-07 18:12:14 +0700

    add width

--- lib/member/profil_rozak.dart
+++ lib/member/profil_rozak.dart
@@ -9,7 +9,9 @@ class MyProfileDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container()
+      body: Container(
+        width: double.infinity,
+      )
     )
   }
 }
