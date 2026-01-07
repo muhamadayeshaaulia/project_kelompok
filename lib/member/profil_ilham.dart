1f8c9c943ab0462200564c82abe10935c1e44802
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-01-07 19:04:38 +0700

    Menambah File Profil Baru

---
+++ lib/member/profil_ilham.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IlhamProfilPage extends StatelessWidget {
+  const IlhamProfilPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
