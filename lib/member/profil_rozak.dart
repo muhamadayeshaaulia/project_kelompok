cb7ad94535a6381e0788af493da3c7149cb53cd9
rozakgit <muhammadabdulrozak21@gmail.com>
2026-01-07 18:10:32 +0700

    buat stl

--- lib/member/profil_rozak.dart
+++ lib/member/profil_rozak.dart
@@ -1,2 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+class MyProfileDetailPage extends StatelessWidget {
+  const MyProfileDetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
