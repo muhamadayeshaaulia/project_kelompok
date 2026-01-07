79f6f47de9f119ca57d0a471c00892d82366bbe4
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-01-07 20:38:04 +0700

    Menambah Row

--- lib/screen/screen2.dart
+++ lib/screen/screen2.dart
@@ -26,6 +26,25 @@ class MyPage3 extends StatelessWidget {
             textAlign: TextAlign.center,
           ),
           SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  height: 20,
+                  width: 20,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+             
+              
+            ],
+          ),
+          SizedBox(height: 20),
           
         ],
       ),
