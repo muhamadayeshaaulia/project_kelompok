bb120cbcbcbad5279424b12d42ee8210ea0789e1
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-01-07 20:37:10 +0700

    Menambah Text

--- lib/screen/screen2.dart
+++ lib/screen/screen2.dart
@@ -20,6 +20,12 @@ class MyPage3 extends StatelessWidget {
             ),
           ),
           SizedBox(height: 20),
+          Text(
+            'Selamat datang di aplikasi Booth-Art \n Aplikasi ini di buat bertujuan untuk tugas UAS pembelajaran Aplikasi mobile menggunakan flutter dan firebase',
+            style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+\
           
         ],
       ),
