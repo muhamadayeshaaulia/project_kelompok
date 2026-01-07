b35cf25cd8afb8a87392e8436f5e3fc7f6dab21a
rozakgit <muhammadabdulrozak21@gmail.com>
2026-01-07 18:29:12 +0700

    update _buildHeader

--- lib/member/profil_rozak.dart
+++ lib/member/profil_rozak.dart
@@ -210,6 +210,22 @@ Widget _buildHeader(Map<String, dynamic> data) {
           letterSpacing: 1,
         ),
       ),
+      const SizedBox(height: 5),
+      Container(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.2),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Text(
+          data['role'] ?? "Mahasiswa",
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 12,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
     ],
   );
 }
