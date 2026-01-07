0ea2719f5ed01dacae7d3494b3feaa31f2438669
rozakgit <muhammadabdulrozak21@gmail.com>
2026-01-07 18:20:29 +0700

    add children

--- lib/member/profil_rozak.dart
+++ lib/member/profil_rozak.dart
@@ -58,7 +58,9 @@ class MyProfileDetailPage extends StatelessWidget {
               );
             }
             var data = snapshot.data!.data() as Map<String, dynamic>;
-            return Column()
+            return Column(
+              children: []
+            )
           },
         ),
       ),
