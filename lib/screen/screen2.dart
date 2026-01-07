75c2af287a654d6496fa389d06d92c0a0a92f0f9
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-01-07 20:38:17 +0700

    Menambah Row2

--- lib/screen/screen2.dart
+++ lib/screen/screen2.dart
@@ -41,7 +41,17 @@ class MyPage3 extends StatelessWidget {
                 ),
               ),
              
-              
+              SizedBox(
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  height: 20,
+                  width: 20,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.blue[100],
+                  ),
+                ),
+              ),
             ],
           ),
           SizedBox(height: 20),
