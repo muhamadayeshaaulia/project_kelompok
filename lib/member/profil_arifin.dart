186742796e0a41296fe940aafddd08f7aa4089ff
Muhammad-ArifinDev <ipin93936@gmail.com>
2026-01-09 21:30:17 +0700

    menambahkan variabel

--- lib/member/profil_arifin.dart
+++ lib/member/profil_arifin.dart
@@ -3,9 +3,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ArifinProfilPage extends StatelessWidget {
   const ArifinProfilPage({super.key});
+  
+  final String arifinDocId = "lpFLoTHeFTPRtfoESKnFoGzO3a42";
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final double headerHeight = 380.0;
+    final double contentStartPos = 340.0;
+
+    return Container(); 
   }
 }
