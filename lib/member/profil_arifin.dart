0de09d16b967a95ff0ffcfd158fa560b3f3cda7d
Muhammad-ArifinDev <ipin93936@gmail.com>
2026-01-09 21:32:36 +0700

    membuat AppBar

--- lib/member/profil_arifin.dart
+++ lib/member/profil_arifin.dart
@@ -14,6 +14,22 @@ class ArifinProfilPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xFFF8F9FA),
       extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Container(
+          margin: const EdgeInsets.only(left: 10, top: 10),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.3),
+            shape: BoxShape.circle,
+            border: Border.all(color: Colors.white.withOpacity(0.5), width: 1),
+          ),
+          child: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () => Navigator.pop(context),
+          ),
+        ),
+      ),
       body: Container(),
     );
   }
